fd8d36c83161c7d53ad043b2e1090e38839974f8
dua <dua@neusta.de>
2023-06-20 11:26:22 +0200

    IDao und IKaufvertrag ergänzt

--- src/Kaufvertrag/businessObjects/IKaufvertrag.java
+++ src/Kaufvertrag/businessObjects/IKaufvertrag.java
@@ -1,12 +1,19 @@
 package Kaufvertrag.businessObjects;
 
 public interface IKaufvertrag {
-public IVertragspartner getVerkaeufer();
-public void setVerkaeufer(IVertragspartner verkaeufer);
-public IVertragspartner getKaeufer();
-public void setKaeufer(IVertragspartner kaeufer);
-public IWare getWare();
-public void setWare(IWare ware);
-public String getZahlungsModalitaeten();
-public void setZahlungsModalitaeten(String zahlungsModalitaeten);
+    public IVertragspartner getVerkaeufer();
+
+    public void setVerkaeufer(IVertragspartner verkaeufer);
+
+    public IVertragspartner getKaeufer();
+
+    public void setKaeufer(IVertragspartner kaeufer);
+
+    public IWare getWare();
+
+    public void setWare(IWare ware);
+
+    public String getZahlungsModalitaeten();
+
+    public void setZahlungsModalitaeten(String zahlungsModalitaeten);
 }
--- src/Kaufvertrag/dataLayer/dataAccessObjects/IDao.java
+++ src/Kaufvertrag/dataLayer/dataAccessObjects/IDao.java
@@ -1,4 +1,17 @@
 package Kaufvertrag.dataLayer.dataAccessObjects;
 
-public interface IDao <T, K>{
+import java.util.List;
+
+public interface IDao<T, K> {
+    public T create();
+
+    public void create(T objectToInsert);
+
+    public T read(K id);
+
+    public List<T> readAll();
+
+    public void update(T objectToUpdate);
+
+    public void delete(K id);
 }
