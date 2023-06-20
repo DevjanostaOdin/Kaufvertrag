ded0cfa9a554793fdae8ad29aa5cf74797aa0f05
dua <dua@neusta.de>
2023-06-20 11:06:47 +0200

    IKaufvertrag und Kaufvertrag erstellt

--- src/Kaufvertrag/dataLayer/businessObjects/Kaufvertrag.java
+++ src/Kaufvertrag/dataLayer/businessObjects/Kaufvertrag.java
@@ -1,6 +1,68 @@
 package Kaufvertrag.dataLayer.businessObjects;
 
 import Kaufvertrag.businessObjects.IKaufvertrag;
+import Kaufvertrag.businessObjects.IVertragspartner;
+import Kaufvertrag.businessObjects.IWare;
 
 public class Kaufvertrag implements IKaufvertrag {
+    IVertragspartner verkaeufer;
+    IVertragspartner kaeufer;
+    IWare ware;
+    String zahlungsModalitaeten;
+
+    public Kaufvertrag(IVertragspartner verkaeufer, IVertragspartner kaeufer, IWare ware) {
+        this.verkaeufer = verkaeufer;
+        this.kaeufer = kaeufer;
+        this.ware = ware;
+    }
+
+    @Override
+    public String toString() {
+        return "Kaufvertrag: " +
+                "Verkäufer: " + verkaeufer +
+                ", Käufer: " + kaeufer +
+                ", Ware: " + ware +
+                ", Zahlungsmodalitäten: '" + zahlungsModalitaeten + '\'' +
+                '.';
+    }
+
+    @Override
+    public IVertragspartner getVerkaeufer() {
+        return verkaeufer;
+    }
+
+    @Override
+    public void setVerkaeufer(IVertragspartner verkaeufer) {
+        this.verkaeufer = verkaeufer;
+    }
+
+    @Override
+    public IVertragspartner getKaeufer() {
+        return kaeufer;
+    }
+
+    @Override
+    public void setKaeufer(IVertragspartner kaeufer) {
+        this.kaeufer = kaeufer;
+    }
+
+    @Override
+    public IWare getWare() {
+        return ware;
+    }
+
+    @Override
+    public void setWare(IWare ware) {
+        this.ware = ware;
+    }
+
+    @Override
+    public String getZahlungsModalitaeten() {
+        return zahlungsModalitaeten;
+    }
+
+    @Override
+    public void setZahlungsModalitaeten(String zahlungsModalitaeten) {
+        this.zahlungsModalitaeten = zahlungsModalitaeten;
+    }
 }
