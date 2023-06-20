fccecb49cee9078fc9e370ec35ea2cf724a1d697
sly <sly@neusta.de>
2023-06-20 10:36:39 +0200

    implemented IWare

--- src/Kaufvertrag/businessObjects/IWare.java
+++ src/Kaufvertrag/businessObjects/IWare.java
@@ -1,4 +1,15 @@
 package Kaufvertrag.businessObjects;
 
+import java.util.List;
+
 public interface IWare {
+    public long getId();
+    public String getBezeichnung();
+    public void setBezeichnung(String bezeichnung);
+    public String getBeschreibung();
+    public void setBeschreibung(String beschreibung);
+    public double getPreis();
+    public void setPreis(double preis);
+    public List<String> getBesonderheiten();
+    public List<String> getMaenge();
 }
