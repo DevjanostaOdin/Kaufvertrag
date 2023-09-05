5646ffd91ff063b8844d04e83a0f94d7ad424e98
sly <sly@neusta.de>
2023-09-05 16:00:17 +0200

    [WIP] xml jdom testen in WareDaoXml

--- src/Kaufvertrag/dataLayer/dataAccessObjects/XML/WareDaoXml.java
+++ src/Kaufvertrag/dataLayer/dataAccessObjects/XML/WareDaoXml.java
@@ -2,36 +2,63 @@ package Kaufvertrag.dataLayer.dataAccessObjects.XML;
 
 import Kaufvertrag.businessObjects.IWare;
 import Kaufvertrag.dataLayer.dataAccessObjects.IDao;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
-import javax.swing.text.Document;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class WareDaoXml implements IDao<IWare, Long> {
 
     @Override
     public IWare create() {
-
-        /*Document doc = new Document() {
-        */
-
-
         return null;
     }
-
-    //addcontent()
-    //
-
-
     @Override
-    public void create(IWare objectToInsert) {
-        WareDaoXml xmlDatei = new WareDaoXml();
+    public void create(IWare objectToInsert)  {
+
+        Document doc = new Document();
+        doc.setRootElement(new Element("Ware"));
+
+        Element ware = new Element("Ware");
+        Element bezeichnung = new Element("Bezeichnung");
+        bezeichnung.setText(objectToInsert.getBezeichnung());
+        Element beschreibung = new Element("Beschreibung");
+        beschreibung.setText(objectToInsert.getBeschreibung());
+        Element preis = new Element("Preis");
+        preis.setText(String.valueOf(objectToInsert.getPreis()));
+        Element besonderheiten = new Element("Besonderheiten");
+        besonderheiten.setText(objectToInsert.getBesonderheiten().toString());
+        Element maengel = new Element("Maengel");
+        maengel.setText(objectToInsert.getMaengel().toString());
+        ware.addContent(bezeichnung);
+        ware.addContent(beschreibung);
+        ware.addContent(preis);
+        ware.addContent(besonderheiten);
+        ware.addContent(maengel);
+
+        doc.getRootElement().addContent(ware);
+
+       try {
+           String datei = String.format("C:\\Users\\sly\\IdeaProjects\\Kaufvertrag\\XMLDateien\\Ware%s.xml", objectToInsert.getId());
+           FileOutputStream fileOutputStream = new FileOutputStream(datei);
+
+           Format format = Format.getPrettyFormat();
+           format.setIndent("    ");
+
+           XMLOutputter xmlOutputter = new XMLOutputter();
+
+           xmlOutputter.setFormat(format);
+           xmlOutputter.output(doc, fileOutputStream);
 
 
-
-        Document doc = xmlDatei.createDoc();
-
-
-
+           // Hier sollte laut Klassendiagramm die ServicXml Klasse aufgerufen werden die eine exception wirft
+        } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
     }
 
     @Override
@@ -45,7 +72,7 @@ public class WareDaoXml implements IDao<IWare, Long> {
     }
 
     @Override
-    public void update(IWare objectTpUpdate) {
+    public void update(IWare objectToUpdate) {
 
     }
 
